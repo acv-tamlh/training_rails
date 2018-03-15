78e4cababa8df4637046f77ced5bc47e51f0b4fe
TamLH <tamlh@atmarkcafe.org>
2018-03-15 17:39:33 +0700

    add validate

---
+++ app/models/product.rb
@@ -0,0 +1,16 @@
+class Product < ApplicationRecord
+	validates :title, :description, :price, presence: true
+
+	validate :title_is_shorter_than_description
+
+	validates_numericality_of :price, greater_than: 0
+
+	def title_is_shorter_than_description
+		return if title.blank? or description.blank?
+		if description.length < title.length
+			errors.add(:description, 'can\'t be shorter than title')
+		end
+	end
+
+
+end

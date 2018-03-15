f63325afe775c6151a76f693c70ae146e42e7544
TamLH <tamlh@atmarkcafe.org>
2018-03-15 15:58:36 +0700

    add belong to 1 category  - n products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315084417) do
+ActiveRecord::Schema.define(version: 20180315085652) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20180315084417) do
     t.datetime "updated_at", null: false
     t.decimal "price", precision: 6, scale: 2
     t.boolean "published"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
 end

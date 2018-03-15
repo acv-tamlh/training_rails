43c9d66905fc7669e02fa14b7d90897f34fa3f40
TamLH <tamlh@atmarkcafe.org>
2018-03-15 15:39:27 +0700

    remove short description in product

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315082436) do
+ActiveRecord::Schema.define(version: 20180315083219) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "price", precision: 6, scale: 2
+    t.boolean "published"
+    t.string "short_description"
   end
 
 end

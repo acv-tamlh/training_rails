0f2b0c04e6c644a0ba598a59767688cb9e69bbe8
TamLH <tamlh@atmarkcafe.org>
2018-03-15 23:15:35 +0700

    add person table, model person, valid email custom

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315091123) do
+ActiveRecord::Schema.define(version: 20180315155659) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "people", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.decimal "age", precision: 10
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

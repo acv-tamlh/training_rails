444a2602c50c520c146ed6c5020ede97d2bca278
TamLH <tamlh@atmarkcafe.org>
2018-03-16 16:27:59 +0700

    add view show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,26 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all.published
+    @products = Product.all.published.includes(:category).references(:category)
+
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    # byebug
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+
   get 'products/index'
 
+  get 'products/show'
+
+  get 'products/new'
+
+  get 'products/create'
+
+  get 'products/edit'
+
+  get 'products/update'
+
+  get 'products/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :products, only: :index
+  resources :products
 end
